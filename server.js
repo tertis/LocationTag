8fb2c3dde49dd647863b207d8e398160e2ed09bf
tertis <tertis@gmail.com>
2015-02-10 23:08:15 +0900

    add html5 index page

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
  * Created by tertis on 15. 2. 10..
  */
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world');
+app.get('*', function(req, res){
+    // sendFile은 절대 경로를 사용해야 한다.
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000);
