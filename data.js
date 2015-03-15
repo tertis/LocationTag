d8bfce9d4527b210b97deac22c7259a283afca9c
tertis <tertis@gmail.com>
2015-03-15 23:48:37 +0900

    [server] delete useless file

--- data.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by tertis on 15. 2. 22..
- */
-// 클래스 형태로 사용.
-var path = require('path');
-
-process.env.port = 80;
-process.env.clientDir = path.join(__dirname, 'public');
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 /**
  * Created by tertis on 15. 2. 10..
  */
-require(__dirname + '/include');
 var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = process.env.PORT || 3000;
+var clientDir = path.join(__dirname, 'public');
+
 // directory를 use해 줘야 angular js의 스크립트가 사용 가능하다.
-app.use(express.static(process.env.clientDir));
+app.use(express.static(clientDir));
 app.get('*', function(req, res){
     // sendFile은 절대 경로를 사용해야 한다.
-    res.sendFile(path.join(process.env.clientDir, 'index.html'));
+    res.sendFile(path.join(clientDir, 'index.html'));
 });
 
-app.listen(process.env.port);
-console.log('Start Listen ' + process.env.port);
+app.listen(port);
+console.log('Start Listen ' + port);
