23bfa7ce958ec5eb8063835a16f62a2c99f99386
tertis <tertis@gmail.com>
2015-04-07 00:33:43 +0900

    [db] change database - back to postgressql of heroku

--- database.js
+++ database.js
@@ -1,17 +1,23 @@
 /**
  * Created by tertis on 15. 2. 15..
  */
-var mysql = require('mysql');
+var pg = require('pg');
 // mysql 설정 파일. 커밋은 하지 않는다.
 var config = require('./config');
 var connectionPool = [];
 
+
 // Create Connection Pool
 exports.initialize = function() {
     for (var i = 0; i < config.connPoolCount; ++i ) {
-        var connection = mysql.createConnection(config.connData);
-        connectionPool.push(connection);
+        var connection = new pg.Client(config.pgConn);
+        connection.connect(function(err) {
+            if(err) {
+                return console.error('could not connect to postgres', err);
+            }
+            connectionPool.push(connection);
+            console.log('[DB] Connection Create...');
+        });
     }
-
-    console.log('[DB] Create Connection Pool.');
+    console.log('[DB] Create Connection Pool. Connection ' + config.connPoolCount);
 }
